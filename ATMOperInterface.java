c6362391a789cf019ca0aeac66cd6ee16fcc46d8
VipashaSable <111556851+VipashaSable@users.noreply.github.com>
2023-04-28 06:48:53 +0530

    Create ATMOperInterface.java

---
+++ ATMOperInterface.java
@@ -0,0 +1,9 @@
+public interface ATMOperationInterface {
+    public void ViewBalance();
+
+    public void WithdrawAmt(double WithdrawAmt);
+
+    public void DepositAmt(double DepositAmt);
+
+    public void ViewMiniStatement();
+}
